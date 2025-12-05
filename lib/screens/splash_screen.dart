7042fc7e2a2e171c15fa582f4fe8af6ed61b0018
Arti-Mdhr <artimanandhar@gmail.com>
2025-12-05 14:01:30 +0545

    logo added

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -12,7 +12,6 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
@@ -26,21 +25,28 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Container(
-        color: Colors.lightGreen, 
-        child: const Center(
-          child: Text(
-            'NepaBite',
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-            ),
+      body: Container(
+        color: const Color.fromARGB(255, 255, 255, 255),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/images/logo.png', height: 180,),
+               const SizedBox(height: 40),
+
+              const Text(
+                'NepaBite',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: const Color.fromARGB(255, 16, 172, 94),
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+            ],
           ),
         ),
       ),
-     
     );
   }
 }
