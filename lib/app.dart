033ce28236bc8ae4ea755452f6ffb6c6d3885437
Arti-Mdhr <artimanandhar@gmail.com>
2025-12-05 09:50:32 +0545

    Initalizing the project

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:nepabite/screens/splash_screen.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NepaBite',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashScreen (),
+    );
+  }
+}
