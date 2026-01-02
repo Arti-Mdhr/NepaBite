766dd65d824c0c1de6e4e7e231a5d4f33d7fab74
Arti-Mdhr <artimanandhar@gmail.com>
2026-01-02 22:01:31 +0545

    Hive service added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:nepabite/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nepabite/app/app.dart';
+import 'package:nepabite/core/services/hive_service.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveService().init();
+  runApp(ProviderScope(child: const MyApp()));
 }
