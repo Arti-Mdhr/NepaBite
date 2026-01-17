c777aef7a869aefbd2c4bcf3daaee7e1b9f102cb
Arti-Mdhr <artimanandhar@gmail.com>
2026-01-17 17:36:20 +0545

    datasoruce added

--- lib/features/auth/data/datasource/auth_datasource.dart
+++ lib/features/auth/data/datasource/auth_datasource.dart
@@ -1,13 +1,21 @@
+import 'package:nepabite/features/auth/data/model/auth_api_model.dart';
 import 'package:nepabite/features/auth/data/model/auth_hive_model.dart';
 
-abstract interface class IAuthDatasource {
-  Future<bool>registerUser(AuthHiveModel model);
+abstract interface class IAuthLocalDatasource {
+  Future<AuthHiveModel> registerUser(AuthHiveModel model);
   Future<AuthHiveModel?> loginUser(String email, String password);
   Future<AuthHiveModel?> getCurrentUser();
-  Future<bool>logout();
+  Future<bool> logout();
 
   // Extra Methods: Doesnt Have to be in DOMAIN LAYER repository
 
-  // Method to check if email exixts
+  // Method to check if email exists
   Future<bool> isEmailExists(String email);
+}
+
+abstract interface class IAuthRemoteDatasource {
+  Future<AuthApiModel> registerUser(AuthApiModel model);
+  Future<AuthApiModel?> getCurrentUser();
+  Future<AuthApiModel?> loginUser(String email, String password);
+  Future<bool> logout();
 }
