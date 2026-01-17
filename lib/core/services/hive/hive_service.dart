6a6b01b043ca330fbe0877b0f9a65f67ee667f56
Arti-Mdhr <artimanandhar@gmail.com>
2026-01-17 14:16:37 +0545

    changes

---
+++ lib/core/services/hive/hive_service.dart
@@ -0,0 +1,76 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:nepabite/core/constants/hive_table_constant.dart';
+import 'package:nepabite/features/auth/data/model/auth_hive_model.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+// Hive Service Provider
+final hiveServiceProvider= Provider<HiveService>((ref){
+  return HiveService();
+});
+
+class HiveService {
+  Future <void>init() async{
+    final directory= await getApplicationDocumentsDirectory();
+
+    final path = '${directory.path}/${HiveTableConstant.dbName}';
+    Hive.init(path);
+    _registerAdapter();
+    await openBoxes();
+  }
+  
+  // Register Adapter
+  void _registerAdapter() {
+    if (!Hive.isAdapterRegistered(HiveTableConstant.authTypeId)) {
+      Hive.registerAdapter(AuthHiveModelAdapter());
+    }
+  }
+  // Open Boxes
+  Future<void> openBoxes() async {
+    await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTable);
+  }
+  // Close Boxes
+  Future<void> close() async{
+    await Hive.close();
+  }
+
+  // Makina a box for user things.
+Box<AuthHiveModel> get _userBox => Hive. box(HiveTableConstant.authTable);
+
+// register user
+Future <AuthHiveModel> registerUser(AuthHiveModel model)async{
+  await _userBox.put(model.authId, model);
+  return model;
+}
+
+// User Login
+Future<AuthHiveModel?> loginUser(String email, String password) async{
+
+  final users= _userBox.values.where(
+    (user)=>user.email == email && user.password==password,
+  );
+  if (users.isNotEmpty) {
+    return users.first;
+  }
+  return null;
+}
+
+// user logout
+Future<void>logout() async{
+
+}
+
+// Get Current User.
+Future<AuthHiveModel?> getCurrentUser(String userId) async {
+  return _userBox.get(userId);
+}
+
+Future<bool> isEmailExists(String email) async {
+  final users= _userBox.values.where((user)=>user.email == email);
+  return users.isNotEmpty;
+}
+}
+
+
+
